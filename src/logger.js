3542c14adaf2d4fc2e61f627053677b4cc20faea
Christopher Nielsen <c.consult.christopher@gmail.com>
2021-11-22 18:30:31 +0100

    changed sampling

--- src/logger.js
+++ src/logger.js
@@ -2,6 +2,7 @@
 const fs = require("fs");
 const SerialPort = require("serialport");
 const ByteLength = require("@serialport/parser-byte-length");
+const Delimiter = require("@serialport/parser-delimiter");
 let cnt = 0;
 
 const wstream = fs.createWriteStream("./data/data.csv", {
@@ -11,18 +12,24 @@ const wstream = fs.createWriteStream("./data/data.csv", {
 
 const ts = new Date();
 wstream.write(`\n${ts.toString()}\n`, () => {
-  const port = new SerialPort("COM5", {
+  const port = new SerialPort("COM4", {
     baudRate: 57600,
+    //baudRate: 125000,
   });
 
-  const parser = port.pipe(new ByteLength({ length: 2 }));
+  //const parser = port.pipe(new ByteLength({ length: 2 }));
+  const parser = port.pipe(
+    new Delimiter({ delimiter: "\n", includeDelimiter: true })
+  );
 
   port.on("error", (err) => {
     console.log("Error", err);
   });
 
   parser.on("data", (data) => {
-    console.log(data.readUIntBE(0, 2));
-    wstream.write(data.readUIntBE(0, 2).toString() + "\n");
+    //console.log(data.toString());
+    wstream.write(data.toString());
+    // console.log(data.readUIntBE(0, 2));
+    // wstream.write(data.readUIntBE(0, 2).toString() + "\n");
   });
 });
